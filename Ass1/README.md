f49badb04d1bdc5d5600724ab0bc19b1e78a4382
phillip28749 <phillip.c.azrael@gmail.com>
2023-02-02 02:52:22 +0800

    textbook

--- Ass1/README.md
+++ Ass1/README.md
@@ -1,4 +1,4 @@
-# CSD3115
+# ass1
 
 
 gcc -std=c99 -pedantic -Wall -Wextra -Wno-deprecated -fno-stack-protector -no-pie -o vpl_execution main.c square-root.s driver-sample.c
---
+++ Ass1/sq.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void sq_root_compute_array(int num_of_elements, unsigned int *array_of_elements) {
+
+    for (int i = 0; i < num_of_elements; i++) {
+
+        unsigned int val = array_of_elements[i];
+        unsigned int root = 0;
+
+        for (unsigned int i = 1; i <= val; i += 2) {
+            root++;
+            val -= i;
+            if (i + 2 > val) {
+                break;
+            }
+        }
+
+        printf("Square root of %u is %u\n", val, root);
+    }
+}
+
+void sq_root_compute_varargs(unsigned int count, ...) {
+
+    va_list ap;
+    va_start(ap, count);
+
+    for (unsigned int i = 0; i < count; i++) {
+
+        unsigned int val = va_arg(ap, unsigned int);
+        unsigned int root = 0;
+
+        for (unsigned int i = 1; i <= val; i += 2) {
+
+            root++;
+            val -= i;
+
+            if (i + 2 > val) {
+                break;
+            }
+        }
+        printf("Square root of %u is %u\n", val, root);
+    }
+
+    va_end(ap);
+}
+
+
+int main(){
+
+    int a[]={ 25, 169, 9810};
+    sq_root_compute_array(3, a);
+    //sq_root_compute_varargs(25, 169, 9810, 0);
+    sq_root_compute_varargs(3, 25, 169, 9810);
+
+    return 0;
+
+
+    
+}
